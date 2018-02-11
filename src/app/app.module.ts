5a21b19a4c762b4618d6d4e32fd9b4a759a34c25
latemba <chalztemba@gmail.com>
2018-02-11 12:38:23 +0300

    Angular bindings and directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './products/productlist.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule
