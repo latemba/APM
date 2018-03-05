5040a6ab67882da52aa5604efabd42094866a7f7
latemba <chalztemba@gmail.com>
2018-03-05 17:13:39 +0300

    two way binding and data piping FormsModule must be imported

--- src/app/products/productlist.component.ts
+++ src/app/products/productlist.component.ts
@@ -10,6 +10,7 @@ export class ProductlistComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  listFilter: string = 'cart';
   products: any[] = [{
     "productId": 1,
     "productName": "Leaf Rake",
